a945751a8a48a4846179c35649c18c7161c7b08c
Rodrigo Rosa <100646597+rodrigocros@users.noreply.github.com>
2024-04-12 22:07:46 -0300

    feat: ajustes program para docker

--- TesteNdd.api/TesteNdd.api/Program.cs
+++ TesteNdd.api/TesteNdd.api/Program.cs
@@ -39,14 +39,9 @@ builder.Services.AddDbContext<TesteNddContext>(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 
